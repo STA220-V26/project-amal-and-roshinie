d214adacba760f3934cb8f7da827c613ca1c2d78
Roshinie <chathudesilva2000@gmail.com>
2026-03-22 10:46:27 +0100

    Modified the targets

--- _targets.R
+++ _targets.R
@@ -16,7 +16,9 @@ pkgs <- c(
   "tidyverse", # Data management
   "data.table", # fast data management
   "fs", # to work wit hthe file system
-  "zip" # manipulate zip files
+  "zip", # manipulate zip files
+  "gt", #for nice tables
+  "quarto"
 )
 
 invisible(lapply(pkgs, library, character.only = TRUE))
@@ -29,7 +31,7 @@ tar_option_set(
 )
 
 # Run the R scripts stored in the R/ folder where your have stored your custom functions:
-tar_source()
+tar_source("draft-roshinie.R")
 # tar_source("other_functions.R") # Source other scripts as needed.
 
 
@@ -51,4 +53,26 @@ unzip(
   zipfile = "data.zip", 
   files = c("data-fixed/patients.csv", "data-fixed/conditions.csv"),
   exdir = "data"
+)
+
+list(
+  # Load Files
+  tar_target(file_patients, "data/data-fixed/patients.csv", format = "file"),
+  tar_target(file_conditions, "data/data-fixed/conditions.csv", format = "file"),
+  
+  # Clean and Flag
+  tar_target(data_patients, clean_patients(file_patients)),
+  tar_target(data_flags, identify_conditions(file_conditions)),
+  
+  # Link
+  tar_target(data_final, left_join(data_patients, data_flags, by = c("id" = "patient")) %>% 
+               mutate(across(starts_with("has_"), ~replace_na(., FALSE)))),
+  
+  # Outputs for Report
+  tar_target(tab_demo, create_demographic_table(data_final)),
+  tar_target(fig_prev, plot_income_prevalence(data_final)),
+  tar_target(fig_interact, plot_demographic_interaction(data_final)),
+  
+  # Render Quarto for the presentation
+  tar_quarto(presentation_slides, "docs/presentation.qmd")
 )
